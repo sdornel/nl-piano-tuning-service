9db86e954f9bb09ddfc016f860a79421888ece9f
kaidornel <kai_dornel@hotmail.com>
2020-08-14 12:35:15 -0400

    playing with format of tuning page

--- src/Components/PianoTuningVideos.js
+++ src/Components/PianoTuningVideos.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import './PianoTuningVideos.css'
+import {Container, Row, Col} from 'react-bootstrap'
 
 const PianoTuningVideos = (props) => {
     return(
         <div className="piano-tuning">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/O6998W0r5Zg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-            </iframe>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/aSXtXLAVgkE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-            </iframe>
+            {/* <Container>
+                <Row>
+                    <Col> */}
+                        {/* <div className="video-left"> */}
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/O6998W0r5Zg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                            </iframe>
+                        {/* </div> */}
+                    {/* </Col>
+                    <Col> */}
+                        {/* <div className="video-right"> */}
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/aSXtXLAVgkE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                            </iframe>
+                        {/* </div> */}
+                    {/* </Col>
+                </Row>
+            </Container> */}
         </div>
     )
 }
