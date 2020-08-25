b2c75218c7c9ebb772d40725d6d6644dd76ea6ae
kaidornel <kai_dornel@hotmail.com>
2020-08-25 00:02:03 -0400

    left notes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,6 +5,8 @@ const Contact = (props) => {
     return (
         <div>
             Contact form
+            {/* check https://medium.com/kirsten-werner/using-a-react-form-to-send-information-directly-to-your-email-2c9666f0d63a */}
+            {/* need to make the form send an email */}
         </div>
     )
 }
